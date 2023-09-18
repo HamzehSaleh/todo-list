bb02d0d5ce690d64766c19597f4656ebafd00fca
hamzeh.saleh <hsaleh@restaurant365.com>
2023-09-18 13:27:23 +0300

    delete all files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList";
+// import TodoForm from "./components/TodoForm";
+// import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      code deleted
+      {/* <TodoList /> */}
     </div>
   );
 }
